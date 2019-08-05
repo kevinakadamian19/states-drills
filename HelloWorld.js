0b1c1e31ab0c1b375db64f22e74f9db30e01bb3f
Kevin Damian Ta <kevindamianta@gmail.com>
2019-08-04 17:50:21 -0700

    Added Buttons
    Added the initial buttons of the skeleton. Next step is to apply the change in state based on event of clicking buttons.

--- HelloWorld.js
+++ HelloWorld.js
@@ -2,12 +2,22 @@ import React from "react";
 
 class HelloWorld extends React.Component {
     constructor() {
+        super()
+
+        this.state = {
+            who: "world"
+        }
 
     }
 
     render() {
         return(
-            
+            <div>
+                <p>Hello, {this.props.who}.</p>
+                <input type="button">World</input>
+                <input type="button">Friend</input>
+                <input type="button">React</input>
+            </div>
         )
     }
 }
