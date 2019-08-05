d148aa63d348834e3f4b23af14385358b7889fb5
Kevin Damian Ta <kevindamianta@gmail.com>
2019-08-04 17:42:54 -0700

    Adding HelloWorld Component
    1st States Drill for states lesson in thinkful. Provided basic skeletal structure of a component first to save.

---
+++ HelloWorld.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class HelloWorld extends React.Component {
+    constructor() {
+
+    }
+
+    render() {
+        return(
+            
+        )
+    }
+}
+
+export default HelloWorld;
